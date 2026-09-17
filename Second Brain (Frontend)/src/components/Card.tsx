1d5d3001ffc1ed0fc0883c3b2696f8b03f03386a
Pranjal Singh Pokhariya <themonkemode@gmail.com>
2026-09-17 10:26:40 +0530

    Made UI improvements to the homepage and added regex for embedding youtube URLs to the card

--- Second Brain (Frontend)/src/components/Card.tsx
+++ Second Brain (Frontend)/src/components/Card.tsx
@@ -1,28 +1,63 @@
 import { ShareIcon } from "../icons/ShareIcon";
 
-export function Card() {
+
+interface CardProps {
+  title: string;
+  type: "github" | "youtube";
+  link: string;
+}
+
+
+function getYouTubeEmbedUrl(rawLink: string) {
+  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\/shorts\/)([^#\&\?]*).*/;
+  const match = rawLink.match(regExp);
+
+  if (match && match[2].length === 11) {
+    return `https://www.youtube.com/embed/${match[2]}`;
+  }
+  
+  return rawLink;
+}
+
+
+export function Card({ title, type, link }: CardProps) {
+  const embedLink = type === "youtube" ? getYouTubeEmbedUrl(link) : link;
+
   return (
     <div>
       <div className="p-4 bg-white rounded-md border-gray-200 max-w-96 border">
 
         <div className="flex justify-between">
-
           <div className="flex items-center text-md">
             <div className="text-gray-500 pr-4">
               <ShareIcon size="md" />
             </div>
-            Project Ideas
+            {title}
           </div>
 
           <div className="flex items-center">
             <div className="pr-2 text-gray-500">
-              <ShareIcon size="md" />
+              <a href={link} target="_blank">
+                <ShareIcon size="md" />
+              </a>
             </div>
             <div className="text-gray-500">
               <ShareIcon size="md" />
             </div>
           </div>
-
+        </div>
+        
+        <div className="pt-4">
+          {type === "youtube" && (
+            <iframe
+              className="w-full rounded-lg"
+              src={embedLink} 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen
+            ></iframe>
+          )}
         </div>
 
       </div>
