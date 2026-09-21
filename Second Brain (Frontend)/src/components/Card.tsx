8b7e5ca98f7d46020f1e7cc96d02a1139bacd874
Pranjal Singh Pokhariya <themonkemode@gmail.com>
2026-09-22 01:57:33 +0530

    Fixed the regex for the Youtube urls

--- Second Brain (Frontend)/src/components/Card.tsx
+++ Second Brain (Frontend)/src/components/Card.tsx
@@ -2,25 +2,23 @@ import { OpenIcon } from "../icons/OpenIcon";
 import { ShareIcon } from "../icons/ShareIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 
-
 interface CardProps {
   title: string;
   type: "github" | "youtube";
   link: string;
 }
 
-
 function getYouTubeEmbedUrl(rawLink: string) {
-  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\/shorts\/)([^#\&\?]*).*/;
+  const regExp =
+    /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|\/shorts\/)([^#&?]*).*/;
   const match = rawLink.match(regExp);
 
   if (match && match[2].length === 11) {
     return `https://www.youtube.com/embed/${match[2]}`;
   }
-  
+
   return rawLink;
 }
-
 
 export function Card({ title, type, link }: CardProps) {
   const embedLink = type === "youtube" ? getYouTubeEmbedUrl(link) : link;
@@ -28,11 +26,10 @@ export function Card({ title, type, link }: CardProps) {
   return (
     <div>
       <div className="p-4 bg-white rounded-md border border-gray-200 max-w-96">
-
         <div className="flex justify-between">
           <div className="flex items-center text-md">
             <div className="text-gray-500 pr-2">
-              <YoutubeIcon size="md" />
+              <YoutubeIcon />
             </div>
             {title}
           </div>
@@ -48,20 +45,19 @@ export function Card({ title, type, link }: CardProps) {
             </div>
           </div>
         </div>
-        
+
         <div className="pt-4">
           {type === "youtube" && (
             <iframe
               className="w-full rounded-lg"
-              src={embedLink} 
-              title="YouTube video player" 
-              frameBorder="0" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              src={embedLink}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
           )}
         </div>
-
       </div>
     </div>
   );
--- Second Brain (Frontend)/src/icons/YoutubeIcon.tsx
+++ Second Brain (Frontend)/src/icons/YoutubeIcon.tsx
@@ -1,4 +1,10 @@
 export function YoutubeIcon() {
-    return <img width="25" height="25" src="https://img.icons8.com/ios-filled/50/youtube-play.png" alt="youtube-play"/>
+  return (
+    <img
+      width="25"
+      height="25"
+      src="https://img.icons8.com/ios-filled/50/youtube-play.png"
+      alt="youtube-play"
+    />
+  );
 }
-
